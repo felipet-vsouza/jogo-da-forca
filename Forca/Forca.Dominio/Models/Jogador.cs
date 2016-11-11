0814180d070c92fd3a23c0d650d1ee5e033bdf84
Unknown <jonathanwssantos1@gmail.com>
2016-11-11 21:43:50 -0200

    feat(Jogador, ContextoDados): adicionar classe jogador e implemtar migration.

---
+++ Forca/Forca.Dominio/Models/Jogador.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forca.Dominio.Models
+{
+    public class Jogador
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public int Pontuacao { get; set; }
+    }
+}
