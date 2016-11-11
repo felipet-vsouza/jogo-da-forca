b7b5af90dc3b1014af5b0a4f2430bc7fce26c03d
Unknown <jonathanwssantos1@gmail.com>
2016-11-11 19:34:53 -0200

    feat(IRepositorio): adicionar interface IRepositorio.

---
+++ Forca/Forca.Dominio/Repositorios/IRepositorioForca.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forca.Dominio.Repositorios
+{
+    public interface IRepositorioForca
+    {
+        void SalvarPontuacaoJogador();
+
+        IEnumerable<Palavra> ListarPalavras();
+    }
+}
--- Forca/Forca.Repositorio/RepositorioForca.cs
+++ Forca/Forca.Repositorio/RepositorioForca.cs
@@ -1,13 +1,23 @@
-﻿using System;
+﻿using Forca.Dominio.Repositorios;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Forca.Dominio;
 
 namespace Forca.Repositorio
 {
-    public class RepositorioForca
+    public class RepositorioForca : IRepositorioForca
     {
+        public IEnumerable<Palavra> ListarPalavras()
+        {
+            throw new NotImplementedException();
+        }
 
+        public void SalvarPontuacaoJogador()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
