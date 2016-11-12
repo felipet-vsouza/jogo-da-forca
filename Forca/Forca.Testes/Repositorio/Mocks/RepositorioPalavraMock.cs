ce75c0b6d2ecc8dd4142ac29b5d94163a2d3bb16
Alexander_Not <matmr.prog@gmail.com>
2016-11-11 23:38:20 -0200

    feat(ServicoPalavraTest, RepositorioPalavrasTest): adicionar testes para repositorio e servico de palavras

---
+++ Forca/Forca.Testes/Repositorio/Mocks/RepositorioPalavraMock.cs
@@ -0,0 +1,25 @@
+﻿using Forca.Dominio;
+using Forca.Dominio.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forca.Testes.Repositorio.Mocks
+{
+    public class RepositorioPalavraMock : IRepositorioPalavra
+    {
+        List<Palavra> listaPalavras = new List<Palavra>
+        {
+            new Palavra { Id = 1, Composicao = "Jeremias"},
+            new Palavra { Id = 2, Composicao = "Josisvaldo"},
+            new Palavra { Id = 3, Composicao = "Jubiscleison" }
+        };
+
+        public IEnumerable<Palavra> ListarPalavras()
+        {
+            return listaPalavras.ToList();
+        }
+    }
+}
---
+++ Forca/Forca.Testes/Repositorio/RepositorioPalavrasTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Forca.Testes.Repositorio.Mocks;
+using Forca.Dominio;
+
+namespace Forca.Testes.Repositorio
+{
+    [TestClass]
+    public class RepositorioPalavrasTest
+    {
+        [TestMethod]
+        public void DeveListarTresPalavras()
+        {
+            RepositorioPalavraMock repositorio = new RepositorioPalavraMock();
+            var lista = repositorio.ListarPalavras();
+
+
+        }
+    }
+}
