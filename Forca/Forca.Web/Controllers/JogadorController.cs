b83e64aab7d5cf3b3ec8fe7dc60e38bf48886ef2
Unknown <jonathanwssantos1@gmail.com>
2016-11-12 12:31:32 -0200

    feat(Jogador): adicionar Jogador controller.

---
+++ Forca/Forca.Web/Controllers/JogadorController.cs
@@ -0,0 +1,40 @@
+﻿using Forca.Dominio.Models;
+using Forca.Dominio.Repositorios;
+using Forca.Repositorio;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace Forca.Web.Controllers
+{
+    public class JogadorController : ApiController
+    {
+        private IRepositorioJogador jogadores = new RepositorioJogador();
+        // GET: Jogador
+        [ResponseType(typeof(Jogador))]
+        public IHttpActionResult PostJogador(Jogador jogador)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            jogadores.SalvarPontuacaoJogador(jogador);
+
+            return Ok();
+        }
+
+        [ResponseType(typeof(Jogador))]
+        public IHttpActionResult GetJogador(int id)
+        {
+            List<Jogador> jogador = jogadores.LeaderRanking().ToList();
+            if (jogador == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(jogador);
+        }
+    }
+}
