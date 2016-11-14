3df01b803aa93460661bd6a0aab4da7601204889
Unknown <felipe.souza@cwi.com.br>
2016-11-14 00:51:04 -0200

    refactor(Palavra.js): refatorar métodos de buscar palavra sorteada e adicionar ao localStorage.

--- Forca/Forca.Web/Scripts/App/Palavra.js
+++ Forca/Forca.Web/Scripts/App/Palavra.js
@@ -1,23 +1,51 @@
 ﻿class Palavra {
 
-    sortearPalavra() {
-        return new Promise((resolve, reject) => {
-            $.get('/api/palavras')
-                .done(function (res) {
-                    var naoSorteadas = res.dados;
-
-                    var indice = Math.ceil(Math.random() * naoSorteadas.length - 1);
-                    var palavraSorteada = naoSorteadas[indice];
     constructor() {
         let self = this;
         this.ready = false;
         this.buscarPalavraSorteada();
     }
 
-                    resolve(palavraSorteada);
-                });
-        });
+    //Método de ligação entre a palavra sorteada e o método que verifica se ela ja foi usada. 
+    buscarPalavraSorteada() {
+        this.sortearPalavra().done(res =>
+            this.adicionarPalavraSorteadaAoLocalStorage(res.id, res.composicao)
+        )
     }
+
+    //Método que faz a requisição da palavra já sorteada 
+    sortearPalavra() {
+        return $.get('/api/palavras');
+    }
+
+    //Método que verifica/adiciona palavras ao localStorage. 
+    adicionarPalavraSorteadaAoLocalStorage(id, composicao) {
+        // Pega os registros que estão no local storage
+        var idExistentes = JSON.parse(localStorage.getItem("idPalavras"));
+        if (idExistentes === null) idExistentes = [];
+        var palavraEncontrada = { Id: id, Composicao: composicao };
+        console.log(palavraEncontrada);
+        var jaFoiUsada = false;
+        //Percorre o array de IDs Existentes: caso o id já esteja registrado retorna ao método de buscarPalavra
+        //senão adiciona novo id e retorna a palavra adicionada
+        for (var ide = 0; ide < idExistentes.length; ide++) {
+            var idE = idExistentes[ide];
+            if (idE === palavraEncontrada.Id) {
+                jaFoiUsada = true;
+            }
+        }
+        if (jaFoiUsada) {
+            this.buscarPalavraSorteada();
+        } else {
+            idExistentes.push(palavraEncontrada.Id);
+            let palavraSorteada = palavraEncontrada.Composicao;
+            localStorage.setItem("idPalavras", JSON.stringify(idExistentes));
+            console.log('PL ' + palavraSorteada);
+            this.palavra = palavraEncontrada.composicao;
+            this.palavraOculta = this.esconderPalavra(palavraEncontrada.composicao);
+            this.ready = true;
+        }
+    };
 
     esconderPalavra(palavra) {
         return palavra.replace(/\S/g, '-');
